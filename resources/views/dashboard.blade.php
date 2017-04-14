abc6f7dff6d5c2da0d22b18bcb1bfa4a77d302d8
Ritwik <ritwiktiwari24@gmail.com>
2017-04-14 22:26:38 +0530

    final commit

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -30,8 +30,8 @@
                         Posted by {{$post->user->name}} on {{ $post->created_at }}
                     </div>
                     <div class="interaction">
-                        <a href="#">Like</a> |
-                        <a href="#">Dislike</a>
+                        <a href="#" class="like">{{ Auth::user()->likes()->where('post_id', $post->id)->first() ? Auth::user()->likes()->where('post_id', $post->id)->first()->like == 1 ? 'You like this post' : 'Like' : 'Like'  }}</a> |
+                        <a href="#" class="like">{{ Auth::user()->likes()->where('post_id', $post->id)->first() ? Auth::user()->likes()->where('post_id', $post->id)->first()->like == 0 ? 'You don\'t like this post' : 'Dislike' : 'Dislike'  }}</a>
                         @if(Auth::user() == $post->user)
                         |
                         <a href="#" class="edit">Edit</a> |
@@ -67,6 +67,7 @@
     </div><!-- /.modal -->
     <script>
         var token = '{{ Session::token() }}';
-        var urlEdit = '{{ route('edit') }}'
+        var urlEdit = '{{ route('edit') }}';
+        var urlLike = '{{ route('like') }}';
     </script>
     @endsection
